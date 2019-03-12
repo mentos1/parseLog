5d97f294ee68916c14482f60319767c054e2b43e
mentos1 <sergkovalov9@yandex.ru>
2019-03-12 23:31:34 +0200

    test

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    /**
+     * Application configuration section
+     * http://pm2.keymetrics.io/docs/usage/application-declaration/
+     */
+    apps: [
+        // First application
+        {
+            name: 'prod eth btc',
+            script: 'bin/www.js',
+            cwd: '/{{pathToProject}}/ico-monitoring',
+            env: {
+                NODE_ENV: 'production',
+                PORT    : '3333',
+
+                EXPERT    : 'Connors RSI Alerts v1_1',
+                LINK_SELL : '34.76.252.86/sell',
+                LINK_BUY  : '34.76.252.86/buy',
+                PATH      : '../Logs',
+
+                HOST      : 'localhost',
+                USER      : 'root',
+                PASSWORD  : 'root',
+                DATABASE  : 'db',
+            },
+            exec_mode: "cluster",
+            instances: 1,
+            log_date_format: "YYYY-MM-DD HH:mm:ss",
+            min_uptime: "15s",
+            max_restarts: 100,
+            max_memory_restart: "400M",
+            cron_restart: "0 1 * * *",
+        }
+    ]
+};
