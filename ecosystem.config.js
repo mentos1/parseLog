015cf61c8ac35c14682241c064386bc597339fcd
mentos1 <kovaloff1995@gmail.com>
2019-03-13 17:26:58 +0200

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
                 EXPERT    : 'Connors RSI Alerts v1_1',
                 LINK_SELL : '34.76.252.86/sell',
                 LINK_BUY  : '34.76.252.86/buy',
-                PATH      : '/Users/lilp13799/AppData/Roaming/MetaQuotes/Terminal/2E8DC23981084565FA3E19C061F586B2/MQL4/Logs',
+                PATH      : '/c/Users/lilp13799/AppData/Roaming/MetaQuotes/Terminal/2E8DC23981084565FA3E19C061F586B2/MQL4/Logs',
             },
             exec_mode: "cluster",
             instances: 1,
